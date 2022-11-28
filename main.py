88ab765b5454c2040bd401a517a49fbe24a65190
Artyom <artemkravtsov2@mail.ru>
2022-11-28 20:53:28 +0300

    update superfunction

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def superfunction():
- print('something')
+ print('1')
 
 def main():
  print('My first git program')
  print('And I change it every day')
  print('Again')
  print('UFO came and added this line')
+ superfunction()
 
 if __name__ == '__main__':
  main()
